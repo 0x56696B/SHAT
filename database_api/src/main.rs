2f6c553fe168ce0610f9da34fb9cd86df9b79c15
Victor Stamoff <transtrike@gmail.com>
2021-07-31 06:56:45 +0300

    Rocket crate added

--- database_api/src/main.rs
+++ database_api/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+//fn main()
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
