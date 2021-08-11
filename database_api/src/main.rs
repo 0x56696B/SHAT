ea598b609918e89e52233cca0a68583b4ddfc346
Victor Stamoff <transtrike@gmail.com>
2021-08-11 22:30:46 +0300

    Fixed? db_recourses clone error

--- database_api/src/main.rs
+++ database_api/src/main.rs
@@ -2,29 +2,23 @@ const PAYLOAD_LIMIT: usize = 4096;
 const IP_ADDR: &str = "127.0.0.1";
 const PORT: &str = "8080";
 
-use actix_web::{dev::Body, error::InternalError, web::JsonConfig, App, HttpResponse, HttpServer};
-use database_api::{
-    db_access::{
-        db_resources::{self, DbResources},
-        db_setup,
-    },
-    endpoints,
-};
+use actix_web::{error::InternalError, web::JsonConfig, App, HttpResponse, HttpServer};
+use database_api::db_access::{db_resources, db_setup};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let conn = db_setup::establish_connection().expect("Unable to make connection to database");
-    let db_recourses = db_resources::DbResources::new(conn);
-
     let server = HttpServer::new(|| {
         let json_config = JsonConfig::default()
             .limit(PAYLOAD_LIMIT)
-            .error_handler(|err, req| {
+            .error_handler(|err, _req| {
                 InternalError::from_response(err, HttpResponse::Conflict().into()).into()
             });
 
         App::new()
-            // .app_data(db_recourses)
+            //Wtf... But nothing else works. since Copy/Clone ain't implemented
+            .app_data(db_resources::DbResources::new(
+                db_setup::establish_connection().expect("Unable to make connection to database"),
+            ))
             .app_data(json_config)
     })
     .bind(format!("{}:{}", IP_ADDR, PORT))?
@@ -32,7 +26,7 @@ async fn main() -> std::io::Result<()> {
     .await;
 
     match server {
-        Ok(success) => Ok(()),
+        Ok(_) => Ok(()),
         Err(err) => Err(err),
     }
 }
